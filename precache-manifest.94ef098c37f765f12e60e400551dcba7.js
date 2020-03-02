70e7d0702ce88d94344423f5f377606c72084645
emmattfu <makcimdjan@gmail.com>
2020-03-02 17:02:11 +0200

    Updates

---
+++ precache-manifest.94ef098c37f765f12e60e400551dcba7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee9402a240131589a6584c15f320791",
+    "url": "/movie/index.html"
+  },
+  {
+    "revision": "e11a64ed5113ffee606a",
+    "url": "/movie/static/css/main.251e5fb4.chunk.css"
+  },
+  {
+    "revision": "3514bbf2384de4ff738e",
+    "url": "/movie/static/js/2.3356949c.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/movie/static/js/2.3356949c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11a64ed5113ffee606a",
+    "url": "/movie/static/js/main.741681ca.chunk.js"
+  },
+  {
+    "revision": "c652e7063a3e25603fd1",
+    "url": "/movie/static/js/runtime-main.944e89b7.js"
+  },
+  {
+    "revision": "422e766543a3850de8db1f049775a1e9",
+    "url": "/movie/static/media/facebook.422e7665.svg"
+  },
+  {
+    "revision": "3bece27512f2d5dc818cb254a975d62b",
+    "url": "/movie/static/media/homepage.3bece275.svg"
+  },
+  {
+    "revision": "3cc0081df83eaaa7743c966de346e481",
+    "url": "/movie/static/media/instagram-social-network-logo-of-photo-camera.3cc0081d.svg"
+  },
+  {
+    "revision": "c95cbd88c68e183c023ca430809dbbdb",
+    "url": "/movie/static/media/twitter.c95cbd88.svg"
+  }
+]);
